870f1a167f1a019af41c2732f07b9752cde224d6
stucopokemontest <143560540+stucopokemontest@users.noreply.github.com>
2024-07-19 01:42:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { Client, Collection, EmbedBuilder } = require('discord.js');
 const { GatewayIntentBits } = require("discord-api-types/v10");
 const { stringSimilarity } = require("string-similarity-js");
 const { Client:YTClient, MusicClient } = require("youtubei");
-const { getRandomIPv6 } = require("@distube/ytdl-core/lib/utils");
 const {
 	joinVoiceChannel,
 	createAudioPlayer,
@@ -116,10 +115,7 @@ const regexFt = /^(.+?)\s*\((.*)\)$/;
 const regexAlphanumeric = /([a-zA-Z0-9]+)/g;
 
 client.playSong = function(url, inst) {
-	const randomIP = ytdl.createAgent(undefined, {
-	  localAddress: getRandomIPv6("2001:2::/48"),
-	});
-	const stream = ytdl(url, {filter: 'audioonly', agent: randomIP});
+	const stream = ytdl(url, {filter: 'audioonly'});
 	const resource = createAudioResource(stream);
 	inst.player.play(resource);
 	return entersState(inst.player, AudioPlayerStatus.Playing, 5000);
@@ -222,10 +218,8 @@ client.playSongList = async function(videos, index, channel, inst) {
 	start = Math.floor(Math.random() * (start+1));
 	console.log("start at " + start.toString());
 	
-	const randomIP = ytdl.createAgent(undefined, {
-	  localAddress: getRandomIPv6("2001:2::/48"),
-	});
-	const stream = ytdl("https://www.youtube.com/watch?v=" + videos[index].id, { quality :"highestvideo", begin: start.toString() + "s", filter: "audioandvideo", agent: randomIP });
+	
+	const stream = ytdl("https://www.youtube.com/watch?v=" + videos[index].id, { quality :"highestvideo", begin: start.toString() + "s", filter: "audioandvideo"});
 	const resource = createAudioResource(stream);
 	/**
 	 * We will now play this to the audio player. By default, the audio player will not play until
